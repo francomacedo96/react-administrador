ec414d74e9e65d50c13d4ef68163965e09abbb2a
francomacedo96 <francomacedo@outlook.com>
2023-06-28 21:15:13 -0300

    hook

--- administrador-react/src/App.jsx
+++ administrador-react/src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
+  const [pacientes, setPacientes] = useState([]);
+
   return (
     <>
       <div className="container mx-auto">
-        <Header />
+        <Header 
+
+
+        />
           <div className="mt-12 md:flex">
             <Formulario/>
             <ListadoPacientes/>
