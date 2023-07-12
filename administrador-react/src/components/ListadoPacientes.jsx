df3079fbb8cd4a2b07dbd26d99e87346287e7779
francomacedo96 <francomacedo@outlook.com>
2023-07-12 10:35:59 -0300

    dinamico

--- administrador-react/src/components/ListadoPacientes.jsx
+++ administrador-react/src/components/ListadoPacientes.jsx
@@ -6,6 +6,9 @@ const ListadoPacientes = ({ pacientes }) => {
     return (
         <div className="md:w-1/2 lg:w-3/5">
 
+            {pacientes && pacientes.length ? (
+
+            <>
             <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
             <p className="text-xl mt-5 mb-10 text-center">
                 Administra tus {""}
@@ -22,9 +25,16 @@ const ListadoPacientes = ({ pacientes }) => {
                     
                 ))}
             </div>
-
-            
-
+            </>
+        ) : (
+            <>
+                <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+                <p className="text-xl mt-5 mb-10 text-center">
+                    Comineza agregando {""}
+                    <span className="text-indigo-600 font-bold">pacientes</span>
+                </p>
+            </>
+        )}
             
         </div>
     )
